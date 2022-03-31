027a05ecd1265ef57c15e6ac001b95852969f455
SaranyaSiripurapu <Saranyasripurapu@gmail.com>
2022-04-01 01:34:48 +0530

    Test case to verify if exact number of records are returned by…

--- src/main/java/payroll/EmployeePayroll.java
+++ src/main/java/payroll/EmployeePayroll.java
@@ -1,74 +1,90 @@
 package payroll;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Enumeration;
+import java.sql.*;
+import java.time.*;
+import java.util.*;
 
 public class EmployeePayroll {
-	// Connection connection; //database connection
-	public String connectToDatabase(String url, String userName, String password) {
-		Connection connection; // database connection
-		String connectionString = null;
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			System.out.println("Driver loaded");
 
-		} catch (ClassNotFoundException e) {
-			throw new IllegalStateException("No drivers loaded ", e);
-		}
-		try {
-			System.out.println("Connectin to " + url);
-			connection = DriverManager.getConnection(url, userName, password);
-			System.out.println(connection);
-			connectionString = connection.toString();
-		} catch (SQLException throwables) {
-			throwables.printStackTrace();
-		}
-		return connectionString;
-	}
+    String name;
+    LocalDate startDate;
+    int salary;
+    int id;
 
-	public ArrayList<String> listDrivers() {
-		Enumeration<Driver> driverList = DriverManager.getDrivers();
-		ArrayList<String> listOfDrivers = new ArrayList<String>();
-		while (driverList.hasMoreElements()) {
-			Driver driver = (Driver) driverList.nextElement();
-			System.out.println("  " + driver.getClass().getName());
-			listOfDrivers.add(driver.getClass().getName());
-		}
-		return listOfDrivers;
-	}
+    public EmployeePayroll() {
 
-	/**
-	 * Main method for manipulation odf JDBC
-	 * 
-	 * @param args - Default Java param (Not used)
-	 */
-	public static void main(String Args[]) {
-		System.out.println("Welcome to payroll service");
+    }
 
-		/**
-		 * The connection URL for the mysql database is
-		 * jdbc:mysql://localhost:3306/payroll_service where jdbc is the API, mysql is
-		 * the database, localhost is the server name on which mysql is running, we may
-		 * also use IP address, 3306 is the port number and payroll_service is the
-		 * database name.
-		 */
-		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_services?allowPublicKeyRetrieval=true&useSSL=false";
-		/**
-		 * The default username for the mysql database is root.
-		 */
-		String userName = "root";
-		/**
-		 * It is the password given by the user at the time of installing the mysql
-		 * database
-		 */
-		String password = "";
-		EmployeePayroll employeePayRollService = new EmployeePayroll();
-		employeePayRollService.connectToDatabase(jdbcURL, userName, password);
-		employeePayRollService.listDrivers();
-	}
+    public EmployeePayroll(String name, LocalDate startDate, int salary, int id) {
+        this.name = name;
+        this.startDate = startDate;
+        this.salary = salary;
+        this.id = id;
+    }
 
+    public ArrayList<EmployeePayroll> readData(String url, String userName, String password) {
+        ArrayList<EmployeePayroll> listOfDataObjects = new ArrayList<EmployeePayroll>();
+        String sql = "select * from employee_payroll";
+        try {
+            Connection connection = this.connectToDatabase(url, userName, password);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                int salary = resultSet.getInt("salary");
+                LocalDate startdate = resultSet.getDate("start").toLocalDate();
+                listOfDataObjects.add(new EmployeePayroll(name, startdate, salary, id));
+            }
+            connection.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return listOfDataObjects;
+    }
+
+    // Connection connection;  //database connection
+    public Connection connectToDatabase(String url, String userName, String password) {
+        Connection connection = null;  //database connection
+        String connectionString = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver loaded");
+
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("No drivers loaded ", e);
+        }
+        try {
+            System.out.println("Connectin to " + url);
+            connection = DriverManager.getConnection(url, userName, password);
+            System.out.println(connection);
+            connectionString = connection.toString();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return connection;
+    }
+
+    public ArrayList<String> listDrivers() {
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        ArrayList<String> listOfDrivers = new ArrayList<String>();
+        while (driverList.hasMoreElements()) {
+            Driver driver = (Driver) driverList.nextElement();
+            System.out.println("  " + driver.getClass().getName());
+            listOfDrivers.add(driver.getClass().getName());
+        }
+        return listOfDrivers;
+    }
+
+    public static void main(String Args[]) {
+        System.out.println("Welcome to payroll service");
+        // localhost is the server name where database exsists or it can be IP address
+        // 3306 is the port number , payroll_services is the database name
+        String jdbcURL = "jdbc:mysql://localhost:3306/employee_payroll_service?allowPublicKeyRetrieval=true&useSSL=false";
+        String userName = "root";
+        String password = "Addtexthere25";
+        EmployeePayroll EmployeePayroll = new EmployeePayroll();
+        EmployeePayroll.connectToDatabase(jdbcURL, userName, password);
+        EmployeePayroll.listDrivers();
+    }
 }
--- src/test/java/payroll/EmployeePayrollTest.java
+++ src/test/java/payroll/EmployeePayrollTest.java
@@ -1,6 +1,7 @@
 package payroll;
 
 import java.io.IOException;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -8,25 +9,35 @@ import java.util.ArrayList;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.*;
 
 public class EmployeePayrollTest {
-    EmployeePayroll employeePayRollService = new EmployeePayroll();
 
-    @Test
-    void whenInitlializedProgramWeShouldBeAbleToLoadSqlDriverClass() {
-        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_services?useSSL=false";
-        String userName = "root";
-        String password = "";
-        String result = employeePayRollService.connectToDatabase(jdbcURL, userName, password);
-        Assert.assertEquals("com.mysql.jdbc.JDBC4Connection@34251ec", result);
-    }
+	EmployeePayroll EmployeePayroll = new EmployeePayroll();
 
+	@Test
+	void whenInitlializedProgramWeShouldBeAbleToLoadSqlDriverClass() {
+		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_services?useSSL=false";
+		String userName = "root";
+		String password = "Addtexthere25";
+		Connection result = EmployeePayroll.connectToDatabase(jdbcURL, userName, password);
+		Assertions.assertEquals("com.mysql.jdbc.JDBC4Connection@34251ec", result);
+	}
 
-    @Test
-    void whenJdbcDriverClassIsLoadedWeShouldBeAbleToObtainListOfDrivers() {
-        ArrayList<String> listOfDriversTest = new ArrayList<String>();
-        listOfDriversTest.add("com.mysql.jdbc.Driver");
-        listOfDriversTest.add("com.mysql.fabric.jdbc.FabricMySQLDriver");
-        Assert.assertEquals(listOfDriversTest, employeePayRollService.listDrivers());
-    }
+	@Test
+	void whenJdbcDriverClassIsLoadedWeShouldBeAbleToObtainListOfDrivers() {
+		ArrayList<String> listOfDriversTest = new ArrayList<>();
+		listOfDriversTest.add("com.mysql.jdbc.Driver");
+		listOfDriversTest.add("com.mysql.fabric.jdbc.FabricMySQLDriver");
+		Assertions.assertEquals(listOfDriversTest, EmployeePayroll.listDrivers());
+	}
+
+	@Test
+	void givenDataBaseTableWeShouldBeAbleToReadAllTheRecords() {
+		String jdbcURL = "jdbc:mysql://localhost:3306/employee_payroll_service?allowPublicKeyRetrieval=true&useSSL=false";
+		String userName = "root";
+		String password = "Addtexthere25";
+		ArrayList<EmployeePayroll> result = EmployeePayroll.readData(jdbcURL, userName, password);
+		Assertions.assertEquals(3, result.size());
+	}
 }
